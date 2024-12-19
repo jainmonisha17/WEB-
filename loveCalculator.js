6f997eb70a51b96944d0d5aca63a02f2ee98a0cc
jain.monisha <61319556+jainmonisha17@users.noreply.github.com>
2024-12-19 11:17:50 +0530

    Create loveCalculator.js

---
+++ loveCalculator.js
@@ -0,0 +1,11 @@
+prompt("What is your name?");
+prompt("What is your crush name?");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1; // 1 - 100
+
+if(loveScore === 100) {
+    alert("Your love score is: " + loveScore + "%" + "You love each other like Kanye loves Kanye");
+} else {
+alert("Your love score is: " + loveScore + "%");
+}
